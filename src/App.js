af9460b754e44ab6ad358308274c9c314ff7b175
Cory House <housecor@gmail.com>
2019-05-08 10:00:24 -0500

    Add table rows

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import CoursesPage from "./CoursesPage";
+import Nav from "./Nav";
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={HomePage} />
+      <Nav />
+      <Route path="/" component={HomePage} exact />
       <Route path="/courses" component={CoursesPage} />
     </Router>
   );
--- src/CoursesPage.js
+++ src/CoursesPage.js
@@ -1,8 +1,35 @@
 import React from "react";
 
 class CoursePage extends React.Component {
+  renderCourseRow(course) {
+    return (
+      <tr key={course.id}>
+        <td>{course.title}</td>
+        <td>{course.category}</td>
+      </tr>
+    );
+  }
+
   render() {
-    return <h1>Courses</h1>;
+    const courses = [
+      { id: 1, title: "Horses for dummies", category: "Pets" },
+      { id: 2, title: "Clean code", category: "Programming" }
+    ];
+
+    return (
+      <>
+        <h1>Courses</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>{courses.map(this.renderCourseRow)}</tbody>
+        </table>
+      </>
+    );
   }
 }
 
